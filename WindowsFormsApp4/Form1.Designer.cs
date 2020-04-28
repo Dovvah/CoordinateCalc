c26925f796154f48dc36d69baca649a24c8b6170
Tim <37161147+Dovvah@users.noreply.github.com>
2020-04-28 00:48:15 -0500

    Actual Commit
    still learning

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.XBox = new System.Windows.Forms.TextBox();
+            this.YBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // XBox
+            // 
+            this.XBox.Location = new System.Drawing.Point(41, 50);
+            this.XBox.Name = "XBox";
+            this.XBox.Size = new System.Drawing.Size(100, 20);
+            this.XBox.TabIndex = 1;
+            this.XBox.Text = "X";
+            this.XBox.TextChanged += new System.EventHandler(this.XBox_TextChanged);
+            // 
+            // YBox
+            // 
+            this.YBox.Location = new System.Drawing.Point(41, 87);
+            this.YBox.Name = "YBox";
+            this.YBox.Size = new System.Drawing.Size(100, 20);
+            this.YBox.TabIndex = 2;
+            this.YBox.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(186, 179);
+            this.Controls.Add(this.YBox);
+            this.Controls.Add(this.XBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Coordinate Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox XBox;
+        private System.Windows.Forms.TextBox YBox;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string calcfunc;
+        private double XValue;
+        private double YValue;
+        private double afterminus32X;
+        private double afterminus32Y;
+    //    private double aftermultiplicationY;
+     //   private double aftermultiplicationX;
+        private void calculatetotal1()
+            
+        {
+            XValue = System.Double.Parse(XBox.Text);
+            YValue = System.Double.Parse(YBox.Text);
+            switch (calcfunc)
+            {
+                case "SubtractX":
+                    afterminus32X = XValue - 32;
+                    break;
+                case "SubtractY":
+                    afterminus32Y = YValue - 32;
+                    break;
+
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //test
+
+            
+
+                calcfunc = "SubtractX";
+                calculatetotal1();
+                calcfunc = "SubtractY";
+            calculatetotal1();
+
+            Convert.ToString(afterminus32X);
+                double outputX = afterminus32X * 533;
+                double outputY = afterminus32Y * 533;
+                double coordinateX = outputX + 266;
+                double coordinateY = outputY + 266;
+
+            int valueX;
+            int valueY;
+            if(Int32.TryParse(YBox.Text,out valueY))
+            if (Int32.TryParse(XBox.Text, out valueX))
+            {
+                    string finalX = coordinateX.ToString();
+                    string finalY = coordinateY.ToString();
+                
+
+
+                    // if X is 0-32 then it's positive
+                    // if y is between 0-32 then it's positive
+                    //if x is between 32-64 it's negative
+                    // if y is between 32-64 it's negative
+                    if (valueX > 32 && valueY > 32)
+                    {
+                        string[] finalY1 = finalY.Split(new[] { "-" }, StringSplitOptions.None);
+                        string[] finalX1 = finalX.Split(new[] { "-" }, StringSplitOptions.None);
+                        Clipboard.SetText(" - " + finalY1[0] + " " + " - " + finalX1[0] + " " + "200");
+                        MessageBox.Show("-" + finalY1[0] + " " + "-" + finalX1[0] + " " + "200");
+                      
+                    }
+                    if (valueX <= 32 && valueY > 32)
+                    {
+                        string[] finalY1 = finalY.Split(new[] { "-" }, StringSplitOptions.None);
+                        string[] finalX1 = finalX.Split(new[] { "-" }, StringSplitOptions.None);
+                        Clipboard.SetText("-" + finalY1[0] + " " + finalX1[1] + " " + "200");
+                        MessageBox.Show("-" + finalY1[0] + " " + finalX1[1] + " " + "200");
+                        
+                    }
+                    if (valueX > 32 && valueY <= 32)
+                    {
+
+                        string[] finalY1 = finalY.Split(new[] { "-" }, StringSplitOptions.None);
+                        string[] finalX1 = finalX.Split(new[] { "-" }, StringSplitOptions.None);
+                        Clipboard.SetText(finalY1[1] + " " + "-" + finalX1[0] + " " + "200");
+                        MessageBox.Show(finalY1[1] + " " + "-" + finalX1[0] + " " + "200");
+                       
+                    }
+                    if (valueX < 32 && valueY < 32)
+                    {
+
+                        string[] finalY1 = finalY.Split(new[] { "-" }, StringSplitOptions.None);
+                        string[] finalX1 = finalX.Split(new[] { "-" }, StringSplitOptions.None);
+                        Clipboard.SetText(finalY1[1] + " " + finalX1[1] + " " + "200");
+                        MessageBox.Show(finalY1[1] + " " + finalX1[1] + " " + "200" );
+                        
+                    }
+                   
+                    if (valueX == 32 && valueY == 32)
+                    {
+                        Clipboard.SetText("-234 -300 200");
+                        MessageBox.Show("-234 -300 200");
+                       
+                    }
+
+
+                 
+                   
+
+
+
+
+
+
+
+                }
+
+
+        }
+
+        private void XBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
